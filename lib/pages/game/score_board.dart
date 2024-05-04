67038453f2afa00494510636d4fc90255b08355a
koto74 <kotoganaiyo@gmail.com>
2024-05-04 17:49:25 +0900

    chore: change O and X to icon

--- lib/pages/game/score_board.dart
+++ lib/pages/game/score_board.dart
@@ -19,6 +19,7 @@ class ScoreBoard extends StatelessWidget {
       children: <Widget>[
         _scoreTile('X', xWins, Colors.blue),
         _scoreTile('O', oWins, Colors.green),
+        _scoreTile('Draws', draws, Colors.grey),
       ],
     );
   }
@@ -26,8 +27,13 @@ class ScoreBoard extends StatelessWidget {
   Widget _scoreTile(String player, int score, Color color) {
     return Column(
       children: <Widget>[
-        Text(player, style: TextStyle(fontSize: 18, color: color)),
-        Text('$score Wins',
+        (player == 'Draws')
+            ? const Icon(Icons.cached, size: 40, color: Colors.grey)
+            : (player == 'X')
+                ? const Icon(Icons.close, size: 40, color: Colors.blue)
+                : const Icon(Icons.radio_button_unchecked,
+                    size: 40, color: Colors.green),
+        Text(player == 'Draws' ? '$score Draws' : '$score Wins',
             style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
       ],
     );
