7dae3a4717fe4e86eb582bf49334f92a83d0dba5
koto74 <kotoganaiyo@gmail.com>
2024-05-04 11:45:52 +0900

    fix: lint error

--- lib/pages/title/title_page.dart
+++ lib/pages/title/title_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../game/game_page.dart';
+import 'package:fluttertictactoe/pages/game/game_page.dart';
 
 void main() => runApp(const TicTacToeApp());
 
